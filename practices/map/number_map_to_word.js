dba758b2900bf22a0c2416190b7739fad2987483
Fei Xie <fei@ThxNo.local>
2018-07-16 21:09:21 +0800

    finish map session.

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,24 @@
 'use strict';
+const chars = "abcdefghijklmnopqrstuvwxyz"
+
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  return collection.map(convert_number_to_char);
 };
+
+function convert_number_to_char(number){
+  let result = ""
+  while(number!==0){
+    let remainder = number % 26;
+    number = parseInt(number/26);
+
+    let letter = chars.charAt(remainder-1);
+    if(remainder === 0){
+      letter = "z"
+      number--;
+    }
+    result =  letter + result;
+  }
+  return result;
+}
 
 module.exports = number_map_to_word;
--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,26 @@
 'use strict';
+
+const chars = "abcdefghijklmnopqrstuvwxyz"
+
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map(convert_number_to_char);
 };
+
+function convert_number_to_char(number){
+  let result = ""
+  while(number!==0){
+    let remainder = number % 26;
+    number = parseInt(number/26);
+
+    let letter = chars.charAt(remainder-1);
+    if(remainder === 0){
+      letter = "z"
+      number--;
+    }
+    result =  letter + result;
+  }
+  return result;
+}
+
 
 module.exports = number_map_to_word_over_26;
