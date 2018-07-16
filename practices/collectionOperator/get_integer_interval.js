ee43f4a5de11b077010beca1ecb773281339c254
Fei Xie <fei@ThxNo.local>
2018-07-16 16:06:29 +0800

    finish collectionOperator without last 3.

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -2,6 +2,19 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  let desc = number_a > number_b ? true : false;
+  let left = number_a > number_b ? number_b : number_a;
+  let right = number_a > number_b ? number_a :number_b;
+
+  let result = range(right-left+1,left);
+  if(desc){
+    result = result.reverse();
+  }
+  return result;
+}
+
+function range(size, startAt=0){
+  return [...Array(size).keys()].map(i=>i+startAt);
 }
 
 module.exports = get_integer_interval;
--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -2,6 +2,20 @@
 
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
+  let desc = number_a > number_b ? true : false;
+  let left = number_a > number_b ? number_b : number_a;
+  let right = number_a > number_b ? number_a :number_b;
+
+  let arr = range(right-left+1,left);
+  let result = arr.filter(elem=>elem%2===0);
+  if(desc){
+    result = result.reverse();
+  }
+  return result;
+}
+
+function range(size, startAt=0){
+  return [...Array(size).keys()].map(i=>i+startAt);
 }
 
 module.exports = get_integer_interval_2;
