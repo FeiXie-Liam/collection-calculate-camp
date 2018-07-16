dacd8508ae2ef8fb493b6c7cc85129a96f0a4998
Fei Xie <fei@ThxNo.local>
2018-07-16 16:52:37 +0800

    finish collectionOperator.

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,40 @@
 'use strict';
 
+const chars = "abcdefghijklmnopqrstuvwxyz"
+
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  let desc = number_a > number_b ? true : false;
+  let left = number_a > number_b ? number_b : number_a;
+  let right = number_a > number_b ? number_a :number_b;
+
+  let arr = range(right-left+1,left);
+  let result = arr.map(convert_number_to_char);
+
+  if(desc){
+    result = result.reverse();
+  }
+  return result;
+}
+
+function convert_number_to_char(number){
+  let result = ""
+  while(number!==0){
+    let remainder = number % 26;
+    number = parseInt(number/26);
+
+    let letter = chars.charAt(remainder-1);
+    if(remainder === 0){
+      letter = "z"
+      number--;
+    }
+    result =  letter + result;
+  }
+  return result;
+}
+
+function range(size, startAt=0){
+  return [...Array(size).keys()].map(i=>i+startAt);
 }
 
 module.exports = get_letter_interval;
--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,6 +2,9 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  return collection_a.concat(
+    collection_b.filter(elem => !collection_a.includes(elem))
+  );
 }
 
 module.exports = get_union;
