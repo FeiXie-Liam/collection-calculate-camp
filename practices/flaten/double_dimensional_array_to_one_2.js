79153c94672868e5e8f6f390ecc43fffd4057e0f
Fei Xie <fei@ThxNo.local>
2018-07-16 20:57:38 +0800

    finish faltten session.

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,14 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  let flatten_arr = [].concat.apply([], collection);
+  let result_arr = []
+  flatten_arr.forEach(elem=>{
+    if(result_arr.includes(elem) === false){
+      result_arr.push(elem)
+    }
+  })
+  return result_arr;
 }
 
 module.exports = double_to_one;
