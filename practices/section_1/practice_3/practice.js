7c231a18120969470bfa7de4caef175c5ff6984e
Fei Xie <fei@ThxNo.local>
2018-07-16 22:35:57 +0800

    finish section1

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,5 +1,7 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  let sub_arr = object_b['value'];
+  return collection_a.filter(elem => sub_arr.includes(elem));
 }
 
 module.exports = collect_same_elements;
