f3a23b4e7fe0708ac4e87a717bf67e39619714fe
Fei Xie <fei@ThxNo.local>
2018-07-16 22:21:10 +0800

    finish ruduce session.

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -2,6 +2,19 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  let res_arr = [];
+  res_arr.push(number);
+  while (number > 0) {
+    number = number - interval;
+    if (Math.abs(number) <= 1e-6) {
+      number = 0;
+      res_arr.push(0);
+    }
+    else {
+      res_arr.push(parseFloat(number.toPrecision(1)));
+    }
+  }
+  return res_arr;
 }
 
 module.exports = spilt_to_zero;
