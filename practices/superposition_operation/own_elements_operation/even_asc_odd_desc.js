5cdbe2f344f5e888c7a66163f7fd6e8ce2757eca
Fei Xie <fei@ThxNo.local>
2018-07-17 09:27:55 +0800

    last session remain one.

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,7 @@
 'use strict';
 var even_asc_odd_desc = function(collection){
-
+    let even_arr = collection.filter(x=>x%2===0).sort((a,b)=>a-b);
+    let odd_arr = collection.filter(x=>x%2!==0).sort((a,b)=>b-a);
+    return even_arr.concat(odd_arr);
 };
 module.exports = even_asc_odd_desc;
--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,23 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
+function rank_by_two_large_one_small(collection) {
   //这里写代码。。。
+  let small_cnt = Math.floor(collection.length / 3);
+  let sorted_arr = collection.sort((a, b) => a - b);
+  let result_arr = [];
+  for (let i = 0; i < collection.length; ++i) {
+    if (i < small_cnt * 3) {
+      if ((i + 1) % 3 !== 0) {
+        result_arr.push(sorted_arr[i + 1]);
+      }
+      else {
+        result_arr.push(sorted_arr[i - 2]);
+      }
+    }
+    else {
+      result_arr.push(sorted_arr[i]);
+    }
+
+  }
+  return result_arr;
 }
 module.exports = rank_by_two_large_one_small;
