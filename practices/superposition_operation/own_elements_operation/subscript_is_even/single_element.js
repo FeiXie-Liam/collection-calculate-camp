fea6b6454acdc11965f08d497982240c38d8857f
Fei Xie <fei@ThxNo.local>
2018-07-17 12:30:41 +0800

    finish own_elements_operation session.

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,26 +1,23 @@
 'use strict';
 var single_element = function (collection) {
-    let even_arr = [];
-    for (let i = 1; i < collection.length; i += 2) {
-        even_arr.push(collection[i]);
-    }
-    let result = [];
+  let even_arr = collection.filter((v, k) => k % 2 === 1);
 
-    let hist = {};
-    even_arr.forEach(elem => {
-        if (elem in hist) {
-            hist[elem]++;
-        } else {
-            hist[elem] = 1;
-        }
-    })
-    let keys = Object.keys(hist);
-    keys.forEach(elem=>{
-        if(hist[elem]===1){
-            result.push(parseInt(elem,10));
-        }
-    })
-    return result;
+  let repeat_arr = [];
+  let result = [];
+
+  even_arr.forEach(elem => {
+    if (repeat_arr.includes(elem) === false) {
+      if (result.includes(elem)) {
+        result.splice(result.indexOf(elem), 1);
+        repeat_arr.push(elem);
+      }
+      else {
+        result.push(elem);
+      }
+    }
+  })
+
+  return result;
 
 };
 module.exports = single_element;
